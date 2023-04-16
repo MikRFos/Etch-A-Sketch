6e97b33e595f76dff030dc69b983033443f977ef
MikRFos <mikrfos@gmail.com>
2023-04-16 10:40:16 -0400

    Give user ability to change the grid size
    Added functions to change the grid size.
    editGrid - Makes a new grid of x rows/columns based on user input
    deleteGrid - Removes the previous grid.
    init - Creates the initial grid of 16x16 divs
    
    html css changes
    removed border from grid
    Added heading that includes the title and the button to change grid size
    added border around the grid container

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 let container = document.querySelector('.container');
+let button = document.querySelector('button');
 
-for(let i = 0; i < 16; i++){
-  let row = document.createElement('div');
-  row.classList.add('grid-row');
-  container.appendChild(row);
-  for(let j = 0; j<16; j++){
-    let square = document.createElement('div');
-    square.classList.add('grid');
-    row.appendChild(square);
+
+button.addEventListener('click', editGrid);
+
+function init(){
+  createGrid(16);
+}
+
+function createGrid(size){
+  //first create the divs that compose the grid
+  for(let i = 0; i < size; i++){
+    let row = document.createElement('div');
+    row.classList.add('grid-row');
+    container.appendChild(row);
+    for(let j = 0; j<size; j++){
+      let square = document.createElement('div');
+      square.classList.add('grid');
+      row.appendChild(square);
+    }
+  }
+
+  //then add an event listener to them so they change on hover
+  let squareList = document.querySelectorAll('.grid');
+  squareList.forEach(((item) => {
+    item.addEventListener('mouseover', (e) => {
+      e.target.classList.add('grid-hover');
+    });
+  }))
+}
+
+function editGrid(){
+  let size = +prompt('How many grids in each row/column woudld \
+  you like. Only enter a single number');
+  if (isNaN(size)){
+    console.log('not a number');
+    return;
+  }
+  deleteGrid();
+  createGrid(size);
+
+}
+
+function deleteGrid(){
+  while(container.firstChild){
+    container.firstChild.remove();
   }
 }
 
-let squareList = document.querySelectorAll('.grid');
-squareList.forEach(((item) => {
-  item.addEventListener('mouseover', (e) => {
-    e.target.classList.add('grid-hover');
-  });
-}))
+init();
